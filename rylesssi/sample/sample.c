689b44f3da4c7a24175e6fc95a64c2923a651c72
Rylee Ngo <ryleengo@linux200.csc.uvic.ca>
2026-05-26 16:48:03 -0700

    add functions

--- rylesssi/sample/sample.c
+++ rylesssi/sample/sample.c
@@ -4,6 +4,39 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+typedef struct background_job {
+	pid_t pid;
+	char cmd[1024];
+	struct background_job *next;
+} background_job;
+
+background_job *background_head = NULL
+volatile sig_atomic_t running = 0; //if there is a foreground process running
+
+
+
+void add_bg_job(pid_t pid, char *cmd) {
+	background_job *new_bg = malloc(sizeof(background_job));
+	new_bg->pid = pid;
+	strcpy(new_bg->cmd, cmd);
+	new_bg->next = background_head;
+	background_head = new_bg;
+}
+
+void sigint_handler(int signal) {
+	if (!running) {
+		printf("\n");
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
+	}
+}
+
+
 int main()
 {
 	char* username;
@@ -12,12 +45,13 @@ int main()
 	char buffer[2048];
 
 	int bailout = 0;
-
+	
 	gethostname(hostname, sizeof(hostname));
 	username = getlogin();
 
 	while (!bailout)
 	{
+
 		getcwd(cwd,sizeof(cwd));
 
 		snprintf(buffer, sizeof(buffer), "%s@%s: %s > ", username, hostname, cwd);
@@ -27,6 +61,10 @@ int main()
 		if (reply == NULL) {
 			printf("\n");
 			break;
+		}
+		
+		if (strlen(reply) > 0) {
+			add_history(reply);
 		}
 
 		if (!strcmp(reply, "bye"))
